764e3511ad6ec61a5ef9efd34276f8fa65312c02
Karina <kary0295@gmail.com>
2015-10-17 17:24:31 -0500

    codigo de un libro

---
+++ Eje2.cpp
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct nodoArbol
+{
+	struct nodoArbol *ptrIzq;
+	int dato;
+	struct nodoArbol *ptrDer;	
+};
+
+typedef struct nodoArbol NodoArbol;
+typedef NodoArbol *ptrNodoArbol;
+
+
+void insertaNodo( ptrNodoArbol *ptrArbol, int valor);
+void inOrden( ptrNodoArbol ptrArbol );
+
+int main()
+{
+	int i; 
+	int elemento;
+	ptrNodoArbol ptrRaiz = NULL;
+	
+	printf("INGRESE UN VALOR ENTERO\n");
+	for(i=0;i<5;i++)
+	{
+		scanf("%d", elemento);
+		insertaNodo( &ptrRaiz, elemento );
+	}
+	
+}
+
+void insertaNodo( ptrNodoArbol *ptrArbol, int valor )
+{
+	if ( *ptrArbol == NULL )
+	{
+		//*ptrArbol = malloc( sizeof( NodoArbol ) );
+		if ( *ptrArbol != NULL )
+		{
+			( *ptrArbol )->dato = valor;
+			( *ptrArbol )->ptrIzq = NULL;
+			( *ptrArbol )->ptrDer = NULL;
+		}
+		else
+		{
+			printf( "no se inserto %d. No hay memoria disponible.\n", valor );		
+		}
+	}
+	else
+	{
+		if ( valor < ( *ptrArbol )->dato )
+		{
+			insertaNodo( &( ( *ptrArbol )->ptrIzq ), valor );
+		}
+		else if( valor > ( *ptrArbol )->dato )
+		{
+			insertaNodo( &( ( *ptrArbol )->ptrDer ), valor );
+		}
+		else
+		{
+			printf( "dup" );
+		}
+	}
+		
+}
+
+void inOrden( ptrNodoArbol ptrArbol )
+{
+	if ( ptrArbol != NULL )
+	{
+		inOrden( ptrArbol->ptrIzq );
+		printf( "%3d", ptrArbol->dato );
+		inOrden( ptrArbol->ptrDer );
+	}
+	
+}
